de6cb43ceecff6a7438b5514e4a539c0bffab832
Justin Cooper <jcooper@chicousd.org>
2022-08-16 14:54:35 -0700

    all functions

--- Move-Computers.ps1
+++ Move-Computers.ps1
@@ -3,46 +3,58 @@ The scripts is to be run every few minutes. Its purpose it to move
 computers (non-server) to a more agreeable OU so that GPO's can be applied
 without extra effort.
 #>
-[cmdletbinding()] 
+[cmdletbinding()]
 param (
- [Parameter(Position = 0, Mandatory = $True)]
- [Alias('DC', 'Server')]
- [ValidateScript( { Test-Connection -ComputerName $_ -Quiet -Count 1 })]
- [string]$DomainController,
- [Parameter(Position = 1, Mandatory = $True)]
- [Alias('ADCred', 'ADCReds')]
+ [Parameter(Mandatory = $True)]
+ [Alias('DCs')]
+ [string[]]$DomainControllers,
+ [Parameter(Mandatory = $True)]
  [System.Management.Automation.PSCredential]$ADCredential,
- [Parameter(Position = 2, Mandatory = $True)]
+ [Parameter(Mandatory = $True)]
  [Alias('SrcOU')]
  [string]$SourceOrgUnitPath,
- [Parameter(Position = 3, Mandatory = $True)]
+ [Parameter(Mandatory = $True)]
  [Alias('TargOU')]
  [string]$TargetOrgUnitPath,
+ [Alias('wi')]
  [switch]$WhatIf
 )
 
-# AD Domain Controller Session
-$adCmdLets = 'Get-ADcomputer', 'Move-ADObject'
-$adSession = New-PSSession -ComputerName $DomainController -Credential $ADCredential
-Import-PSSession -Session $adSession -Module ActiveDirectory -CommandName $adCmdLets -AllowClobber
-
-. .\lib\Add-Log.ps1
-
-$endTime = "11:30pm"
-if (!$WhatIf) { "Running until $endTime" }
-do {
- $computerObjs = Get-ADcomputer -Filter * -SearchBase $SourceOrgUnitPath -Properties * |
+function Get-NewADComputers {
+ Get-ADcomputer -Filter * -SearchBase $SourceOrgUnitPath -Properties * |
  Where-Object { $_.OperatingSystem -notlike "*Server*" -and { $_.Description -notlike "*Server*" } }
- foreach ($obj in $computerObjs) {
-  # Move Object to TargetPath
-  Add-Log action "Moving $($obj.name) to $($TargetOrgUnitPath.split(",")[0])" $WhatIf
-  Move-ADObject -Identity $obj.ObjectGUID -TargetPath $TargetOrgUnitPath -Whatif:$WhatIf
-  # Loop every 5 minutes
- }
- Write-Verbose "Next run at $((Get-Date).AddSeconds(180))"
- if (!$WhatIf) { Start-Sleep -Seconds 180 }
-} until ( $WhatIf -or ( (Get-Date) -ge (Get-Date $endTime) ) )
+}
 
-Write-Verbose "Tearing down sessions"
-Get-PSSession | Remove-PSSession
- 
+function Move-NewADComputers {
+ process {
+  $msgVars = $MyInvocation.MyCommand.Name, $_.name, $TargetOrgUnitPath.split(",")[0]
+  Write-Host ('{0},{1},{2}' -f $msgVars )
+  Move-ADObject -Identity $_.ObjectGUID -TargetPath $TargetOrgUnitPath -Whatif:$WhatIf
+ }
+}
+
+function New-ADSession ([string[]]$cmdlets, $dc) {
+ $adSession = New-PSSession -ComputerName $dc -Credential $ADCredential
+ Import-PSSession -Session $adSession -Module ActiveDirectory -CommandName $cmdlets -AllowClobber
+}
+
+
+function Move-NewObjectsLoop {
+ if ( (Get-Date) -ge (Get-Date '11:30pm')) { return }
+ Clear-SessionData
+ $dc = Select-DomainController $DomainControllers
+ New-ADSession -cmdlets 'Get-ADcomputer', 'Move-ADObject' -dc $dc
+ Get-NewADComputers | Move-NewADComputers
+ if ($WhatIf) { return }
+ Write-Verbose "Next run at $((Get-Date).AddSeconds(180))"
+ Start-Sleep -Seconds 180
+ Move-NewObjectsLoop
+}
+
+# main
+. .\lib\Clear-SessionData.ps1
+. .\lib\Select-DomainController.ps1
+. .\lib\Show-TestRun.ps1
+
+Show-TestRun
+Move-NewObjectsLoop
