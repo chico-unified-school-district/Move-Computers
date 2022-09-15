1099184be58fd052bfbaf9698256c3eeed2cf010
Justin Cooper <jcooper@chicousd.org>
2022-09-15 13:49:20 -0700

    added servers

--- Move-Computers.ps1
+++ Move-Computers.ps1
@@ -11,11 +11,13 @@ param (
  [Parameter(Mandatory = $True)]
  [System.Management.Automation.PSCredential]$ADCredential,
  [Parameter(Mandatory = $True)]
- [Alias('SrcOU')]
+ [Alias('srcOU')]
  [string]$SourceOrgUnitPath,
  [Parameter(Mandatory = $True)]
- [Alias('TargOU')]
- [string]$TargetOrgUnitPath,
+ [Alias('compOU')]
+ [string]$CompOrgUnitPath,
+ [Alias('serverOU')]
+ [string]$ServerOrgUnitPath,
  [Alias('wi')]
  [switch]$WhatIf
 )
@@ -24,12 +26,24 @@ function Get-NewADComputers {
  Get-ADcomputer -Filter * -SearchBase $SourceOrgUnitPath -Properties * |
  Where-Object { $_.OperatingSystem -notlike "*Server*" -and { $_.Description -notlike "*Server*" } }
 }
+function Get-NewADServers {
+ Get-ADcomputer -Filter * -SearchBase $SourceOrgUnitPath -Properties * |
+ Where-Object { $_.OperatingSystem -like "*Server*" }
+}
 
 function Move-NewADComputers {
  process {
-  $msgVars = $MyInvocation.MyCommand.Name, $_.name, $TargetOrgUnitPath.split(",")[0]
-  Write-Host ('{0},{1},{2}' -f $msgVars )
-  Move-ADObject -Identity $_.ObjectGUID -TargetPath $TargetOrgUnitPath -Whatif:$WhatIf
+  $msgVars = $MyInvocation.MyCommand.Name, $_.name, $CompOrgUnitPath.split(",")[0]
+  Write-Host ('{0},{1},{2}' -f $msgVars ) -Fore Blue
+  Move-ADObject -Identity $_.ObjectGUID -TargetPath $CompOrgUnitPath -Whatif:$WhatIf
+ }
+}
+
+function Move-NewADServers {
+ process {
+  $msgVars = $MyInvocation.MyCommand.Name, $_.name, $ServerOrgUnitPath.split(",")[0]
+  Write-Host ('{0},{1},{2}' -f $msgVars ) -Fore Green
+  Move-ADObject -Identity $_.ObjectGUID -TargetPath $ServerOrgUnitPath -Whatif:$WhatIf
  }
 }
 
@@ -43,8 +57,9 @@ function Move-NewObjectsLoop {
  if ( (Get-Date) -ge (Get-Date '11:30pm')) { return }
  Clear-SessionData
  $dc = Select-DomainController $DomainControllers
- New-ADSession -cmdlets 'Get-ADcomputer', 'Move-ADObject' -dc $dc
+ New-ADSession -cmdlets 'Get-ADComputer', 'Move-ADObject' -dc $dc
  Get-NewADComputers | Move-NewADComputers
+ Get-NewADServers | Move-NewADServers
  if ($WhatIf) { return }
  Write-Verbose "Next run at $((Get-Date).AddSeconds(180))"
  Start-Sleep -Seconds 180
